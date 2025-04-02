09826307277acd83db37774a47a3f1b631268493
Sotatek-HienHa <hien.ha@sotatek.com>
2025-04-02 14:19:24 +0700

    add Coverage Test Report

---
+++ tests/build/CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTests.dir/gtest.cpp.o"
+  "CMakeFiles/runTests.dir/ringbuffer.c.o"
+  "runTests"
+  "runTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
