c68239a38a8c341b48764eaec814530f7e41add7
Sotatek-HienHa <hien.ha@sotatek.com>
2025-04-02 15:30:34 +0700

    add thread safety

--- src/ringbuffer.c
+++ src/ringbuffer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include "ringbuffer.h"
 
 #define MAX_BUFFER 256
@@ -11,49 +12,63 @@ typedef struct {
     unsigned short head;
     unsigned short tail;
     unsigned short count;
+    pthread_mutex_t lock;
 } RingBuffer;
 
 static RingBuffer rb;
 
 void ringbuffer_init(unsigned short size) {
-    if(size >= MAX_BUFFER) {
-        return;
-    }
     rb.size = size;
     memset(rb.buffer, 0, sizeof(int) * size);
     rb.head = 0;
     rb.tail = 0;
     rb.count = 0;
+    pthread_mutex_init(&rb.lock, NULL)
 }
 
 int ringbuffer_isfull() {
-    return rb.count == rb.size;
+    pthread_mutex_lock(&rb.lock);
+    int res = rb.count == rb.size;
+    pthread_mutex_unlock(&rb.lock);
+    return res;
 }
 
 int ringbuffer_isempty() {
-    return rb.count == 0;
+    pthread_mutex_lock(&rb.lock);
+    int res = rb.count == 0;
+    pthread_mutex_unlock(&rb.lock);
+    return res;
 }
 
 int ringbuffer_add(int value) {
+    pthread_mutex_lock(&rb.lock);
     if (ringbuffer_isfull()) {
+        pthread_mutex_unlock(&rb.lock);
         return -1; 
     }
+    rb.buffer[rb.head] = value;
+    rb.head = (rb.head + 1) % rb.size;
     rb.count++;
-    rb.tail = (rb.head + rb.count - 1) % (rb.size -1);
-    rb.buffer[rb.tail] = value;
+    pthread_mutex_unlock(&rb.lock);
     return 0;
 }
 
 int ringbuffer_remove(int *value) {
+    pthread_mutex_lock(&rb.lock);
     if (ringbuffer_isempty()) {
+        pthread_mutex_unlock(&rb.lock);
         return -1;
     }
+    *value = rb.buffer[rb.tail];
+    rb.tail = (rb.tail + 1) % rb.size;
     rb.count--;
-    *value = rb.buffer[rb.head];
-    rb.head = (rb.head + 1) % (rb.size - 1);
+    pthread_mutex_unlock(&rb.lock);
     return 0;
 }
 
 int ringbuffer_size() {
-    return rb.count;
+    pthread_mutex_lock(&rb.lock);
+    int res = rb.count;
+    pthread_mutex_ulock(&rb.lock);
+    return res;
 }
--- tests/ringbuffer.c
+++ tests/ringbuffer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include "ringbuffer.h"
 
 #define MAX_BUFFER 256
@@ -11,6 +12,7 @@ typedef struct {
     unsigned short head;
     unsigned short tail;
     unsigned short count;
+    pthread_mutex_t lock;
 } RingBuffer;
 
 static RingBuffer rb;
@@ -21,36 +23,52 @@ void ringbuffer_init(unsigned short size) {
     rb.head = 0;
     rb.tail = 0;
     rb.count = 0;
+    pthread_mutex_init(&rb.lock, NULL)
 }
 
 int ringbuffer_isfull() {
-    return rb.count == rb.size;
+    pthread_mutex_lock(&rb.lock);
+    int res = rb.count == rb.size;
+    pthread_mutex_unlock(&rb.lock);
+    return res;
 }
 
 int ringbuffer_isempty() {
-    return rb.count == 0;
+    pthread_mutex_lock(&rb.lock);
+    int res = rb.count == 0;
+    pthread_mutex_unlock(&rb.lock);
+    return res;
 }
 
 int ringbuffer_add(int value) {
+    pthread_mutex_lock(&rb.lock);
     if (ringbuffer_isfull()) {
+        pthread_mutex_unlock(&rb.lock);
         return -1; 
     }
     rb.buffer[rb.head] = value;
     rb.head = (rb.head + 1) % rb.size;
     rb.count++;
+    pthread_mutex_unlock(&rb.lock);
     return 0;
 }
 
 int ringbuffer_remove(int *value) {
+    pthread_mutex_lock(&rb.lock);
     if (ringbuffer_isempty()) {
+        pthread_mutex_unlock(&rb.lock);
         return -1;
     }
     *value = rb.buffer[rb.tail];
     rb.tail = (rb.tail + 1) % rb.size;
     rb.count--;
+    pthread_mutex_unlock(&rb.lock);
     return 0;
 }
 
 int ringbuffer_size() {
-    return rb.count;
+    pthread_mutex_lock(&rb.lock);
+    int res = rb.count;
+    pthread_mutex_ulock(&rb.lock);
+    return res;
 }
